078ae69662a354aa4992b87cab3b7f237da4e889
witoldzol <blue3656@gmail.com>
2017-07-31 06:01:46 +0100

    working on timer

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-let timer = setInterval(myTimer, 100)
 
-
+//element displaying main clock
 let clock = document.getElementById("clock")
 
+let startDate = new Date()
 
-alert(clock.innerHTML)
+let pomodoroClock = 25
+
+let timer = startDate.setMinutes(pomodoroClock).setSeconds(0)
+
+let sec = timer.getSeconds()
+let min = timer.getMinutes()
+
+alert(sec)
+alert(min)
+let counter = function () {
+
+	sec--
+	if(sec == 00) {
+		sec = 60
+		min--
+	}
+	clock.innerHTML = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec)
+
+	
+}
+
+counter()
+
+setInterval(counter, 1000)	
+
+
+
+
