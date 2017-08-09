c3d0e4bc8a0001f359feeaae485f3af20707f2b7
witoldzol <blue3656@gmail.com>
2017-08-09 05:49:22 +0100

    start timer button

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
 
+//------OBJECTS
+
+//start button
+let startBtn = document.getElementById('start')
+let flag = 0
 
 //element displaying main clock
 let clock = document.getElementById("clock")
 
-let duration = 25 * 60
+//default values
+let pomodoroTime = 25
+let pomodoroBrake = 5
 
+//duration in seconds
+let duration = pomodoroTime * 60
+
+//timer function
 function startTimer(duration, display) {
 
 
-    var timer = duration
+    let timer = duration
 
-    setInterval(function () {
+    let timerFunction = setInterval(function () {
 
         minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+        seconds = parseInt(timer % 60, 10)
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = minutes + ":" + seconds
 
         if (--timer < 0) {
-            timer = duration;
+            timer = duration
         }
-    }, 1000);
+    }, 1000)
 }
-startTimer(duration, clock)
+
+//start&pause timer and change button name 
+function starter () {
+
+    
+
+    if (flag === 0) {
+
+        flag = 1
+        startTimer(duration, clock)
+    } 
+    else {
+
+        flag = 0 
+        clearInterval(timerFunction);
+    }
+
+}
+
+//start button on click
+startBtn.onclick = starter()
+
+// to do:
+
+-fix the pausing 
+-do the name change on the  button
+-make an loop between pomodoro timer and brake timer (transition and auto reset)
